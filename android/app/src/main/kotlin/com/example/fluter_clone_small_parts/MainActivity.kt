a6ba34356dfb3c09129863a5d6ad7868c2df1ce3
realryankim <ryankimofficial@gmail.com>
2021-11-23 16:51:40 +0900

    initial setting

---
+++ android/app/src/main/kotlin/com/example/fluter_clone_small_parts/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluter_clone_small_parts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:fluter_clone_small_parts/pages/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Clone Small Parts',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: '/',
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => MainScreen(),
+          transition: Transition.noTransition,
+        ),
+      ],
+    );
+  }
+}
